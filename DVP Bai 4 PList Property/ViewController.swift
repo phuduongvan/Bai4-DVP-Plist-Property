96407b6d067d2dca9bed758a02c4112fd9175c7f
THANH <thanh@THANHs-Mac.local>
2017-05-11 21:06:20 +0700

    Initial Commit

---
+++ DVP Bai 4 PList Property/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DVP Bai 4 PList Property
+//
+//  Created by THANH on 5/11/17.
+//  Copyright © 2017 HCMUTE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
